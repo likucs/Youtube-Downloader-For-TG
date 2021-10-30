d141a1ebdfbe6cdd828c747ec144b070c1f87d39
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-10-31 02:46:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,6 +10,6 @@ async def start(client, message):
         [InlineKeyboardButton("📌️ support 🔎", url="https://t.me/KOT_REPORS")]
 
     ])
-    thumbnail_url = "https://telegra.ph/file/7228b162849378a99a635.jpg"
+    thumbnail_url = "https://telegra.ph/file/fe4379cb5ebf812e3379e.jpg"
     await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
