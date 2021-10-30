7441ab58ae1bd51e7f88208901584b4d3a75aaf8
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2021-10-31 02:44:51 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,9 +4,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	alpha2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("⚡ Channel ", url="https://t.me/JNS_BOTS")],
-		[InlineKeyboardButton("Report Bugs 🚨", url="https://t.me/JNS_FC_BOTS")]
+		[InlineKeyboardButton("⚡ Channel ", url="https://t.me/KOT_BOTS")],
+		[InlineKeyboardButton("Report Bugs 🚨", url="https://t.me/KOT_REPORS")]
                 ])
 
 	help_image = "https://telegra.ph/file/7228b162849378a99a635.jpg"
-	await message.reply_photo(help_image,  caption="**💡 HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By JNS BOTS``` 🚨\n",reply_markup=alpha2)
+	await message.reply_photo(help_image,  caption="**💡 HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By @KOT_BOTS``` 🚨\n",reply_markup=alpha2)
