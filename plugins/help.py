376537ff701a6848ec38b878405bb5aac4e4840a
Liku cs <83144124+likucs@users.noreply.github.com>
2022-04-10 15:45:24 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -4,9 +4,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
 	alpha2 = InlineKeyboardMarkup([
-		[InlineKeyboardButton("⚡ Channel ", url="https://t.me/KOT_BOTS")],
-		[InlineKeyboardButton("Report Bugs 🚨", url="https://t.me/KOT_REPORS")]
+		[InlineKeyboardButton("⚡ Channel ", url="https://t.me/cs_cloud")],
+		[InlineKeyboardButton("Report Bugs 🚨", url="https://t.me/discuss_group_cs")]
                 ])
 
-	help_image = "https://telegra.ph/file/94b0207fe12540dd68b08.jpg"
+	help_image = "https://telegra.ph/file/5dbcf92eb25bd86595990.jpg"
 	await message.reply_photo(help_image,  caption="**💡 HELP 📃...**\n \n__• Just Send your Youtube video url 🌟__ \n__• And i will give Method list to select your choice 😋__\n \n======================\n__• 😊 This bot is fully free.__\n`•⚙ Don't pay anyone for Bots like this.`\n\n⚡ ```Bot By @KOT_BOTS``` 🚨\n",reply_markup=alpha2)
