19eb20d64c913ff774a92baca367fdc38598a159
Liku cs <83144124+likucs@users.noreply.github.com>
2022-04-10 15:44:02 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,10 @@ async def start(client, message):
     # return
     Alpha = InlineKeyboardMarkup([
         
-        [InlineKeyboardButton("📌️ KOT BOTS 🔎", url="https://t.me/KOT_BOTS")],
-        [InlineKeyboardButton("📌️ support 🔎", url="https://t.me/KOT_REPORS")]
+        [InlineKeyboardButton("🤖 CS BOTS 🔎", url="https://t.me/cs_cloud")],
+        [InlineKeyboardButton("☁️ support 🔎", url="https://t.me/discuss_group_cs")]
 
     ])
-    thumbnail_url = "https://telegra.ph/file/94b0207fe12540dd68b08.jpg"
+    thumbnail_url = "https://telegra.ph/file/5dbcf92eb25bd86595990.jpg"
     await message.reply_photo(thumbnail_url, caption=f"**🙂 Hi <b>{message.from_user.first_name}**</b>\n\n<br>__😇 I Can Download YT Videos For You ✨️__</br>\n\n<b>• **🗂️ Instructions for use...🚨**</b>\n• **⚙ Type /help to get instructins...**\n \n───── ❝ **Lets Play** ❞ ─────\n ", reply_markup=Alpha)
     raise StopPropagation
